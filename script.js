a907a9814abe089462a7829d983f6259c9097410
unknown <mosesonyango724@gmail.com>
2023-11-30 09:56:39 +0300

    deploy

---
+++ script.js
@@ -0,0 +1,28 @@
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const seconds = document.getElementById('seconds');
+
+
+const clock = setInterval(function time() {
+    let dateToday =  new Date(); 
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes(); 
+
+    let sec = dateToday.getSeconds();
+    console.log(hr, min, sec);
+    if (hr < 10) {
+        hr = '0' + hr;
+        
+    }
+    if (min < 10) {
+        min = '0' + min;
+    }
+
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+    hour.textContent = hr;
+    minute.textContent = min;
+    seconds.textContent = sec;
+    
+}, 1000);
